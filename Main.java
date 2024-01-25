0e97ceef5708085f1e16277b3308ed01649bfb6a
vor3743 <89716200+vor3743@users.noreply.github.com>
2024-01-25 08:57:40 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
   public static void main(Strings[] args) {
-    System.out.println("Hello Ravens!");
+    System.out.println("Hello Ravens! Let's practice some code!");
   }
 }
